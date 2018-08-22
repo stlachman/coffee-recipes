59ece0c705a8fbb039f5f13d6900231cc02d8084
Seth Lachman <lachmanseth17@gmail.com>
2018-08-21 20:45:42 -0400

    Added custom settings for each range based on the ratio

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 const gramRange = Array.prototype.slice.call(document.querySelectorAll(".gram-range"))
 gramRange.forEach(range => {
     range.addEventListener("change", function getRange() {
-        console.log(this);
+        // console.log(this);
         sliderChange(this);
     });
 });
+
+const v60Ratio = 15;
+const frenchPressRatio = 18;
+const chemexRatio = 17;
 
 function sliderChange(e){
    var index = document.getElementsByTagName('input');
    var sliders = document.getElementsByClassName('coffee-gram');
    var waterValue  = document.getElementsByClassName('water-ml');
    for (i = 0; i < index.length; i++){
-      if(e == index[i]){
+       console.log(e.id);
+      if(e == index[i] && e.id == 'v60-ratio'){
          sliders[i].innerHTML = e.value + ' grams';
-         waterValue[i].innerHTML = Math.ceil(e.value * 16.36) + ' ml';
+         waterValue[i].innerHTML = Math.ceil(e.value * v60Ratio) + ' ml';
+      }  else if(e == index[i] && e.id == 'frenchpress-ratio') {
+        sliders[i].innerHTML = e.value + ' grams';
+        waterValue[i].innerHTML = Math.ceil(e.value * frenchPressRatio) + ' ml';
+      } else if(e == index[i] && e.id == 'chemex-ratio') {
+        sliders[i].innerHTML = e.value + ' grams';
+        waterValue[i].innerHTML = Math.ceil(e.value * chemexRatio) + ' ml';
       }
    }
 }
