a549838156a0da51bdb9ed7106aae6a56e15dbc5
Seth Lachman <lachmanseth17@gmail.com>
2018-08-22 21:51:34 -0400

    Updatd some styles and started working on timer feature.

--- js/main.js
+++ js/main.js
@@ -28,5 +28,19 @@ function sliderChange(e){
    }
 }
 
+// function startTimer(){
+//     var sec = 30;
+//     var timer = setInterval(function(){
+//         document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+//         sec--;
+//         if (sec < 0) {
+//             clearInterval(timer);
+//         }
+//     }, 1000);
+// }
+
+const timerButton = document.querySelector(".timer"); 
+timerButton.addEventListener("click",  startTimer());
+
 
 
