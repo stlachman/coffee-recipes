ecf34e4aa9e46f5e5c0fb1322bd33c41448a3214
Seth Lachman <lachmanseth17@gmail.com>
2018-08-23 21:24:12 -0400

    Added timer for coffee brewing

--- js/main.js
+++ js/main.js
@@ -28,19 +28,79 @@ function sliderChange(e){
    }
 }
 
-// function startTimer(){
-//     var sec = 30;
-//     var timer = setInterval(function(){
-//         document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
-//         sec--;
-//         if (sec < 0) {
-//             clearInterval(timer);
-//         }
-//     }, 1000);
-// }
+function CountDownTimer(duration, granularity) {
+  this.duration = duration;
+  this.granularity = granularity || 1000;
+  this.tickFtns = [];
+  this.running = false;
+}
 
-const timerButton = document.querySelector(".timer"); 
-timerButton.addEventListener("click",  startTimer());
+CountDownTimer.prototype.start = function() {
+  if (this.running) {
+    return;
+  }
+  this.running = true;
+  var start = Date.now(),
+      that = this,
+      diff, obj;
+
+  (function timer() {
+    diff = that.duration - (((Date.now() - start) / 1000) | 0);
+
+    if (diff > 0) {
+      setTimeout(timer, that.granularity);
+    } else {
+      diff = 0;
+      that.running = false;
+    }
+
+    obj = CountDownTimer.parse(diff);
+    that.tickFtns.forEach(function(ftn) {
+      ftn.call(this, obj.minutes, obj.seconds);
+    }, that);
+  }());
+};
+
+CountDownTimer.prototype.onTick = function(ftn) {
+  if (typeof ftn === 'function') {
+    this.tickFtns.push(ftn);
+  }
+  return this;
+};
+
+CountDownTimer.prototype.expired = function() {
+  return !this.running;
+};
+
+CountDownTimer.parse = function(seconds) {
+  return {
+    'minutes': (seconds / 60) | 0,
+    'seconds': (seconds % 60) | 0
+  };
+};
+
+window.onload = function () {
+    var display = document.querySelector('#safeTimerDisplay'),
+        timer = new CountDownTimer(60 * 3),
+        timeObj = CountDownTimer.parse(60 * 3);
+
+    format(timeObj.minutes, timeObj.seconds);
+    
+    timer.onTick(format);
+    
+    document.querySelector('.timer').addEventListener('click', function () {
+        timer.start();
+    });
+    
+    function format(minutes, seconds) {
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ':' + seconds;
+    }
+};
+
+
+
 
 
 
